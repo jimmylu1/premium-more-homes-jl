ae0e7e13a829f16d880e5e9d96d07a8807a21a26
jimmylu1 <jimmy88lu@gmail.com>
2019-06-06 21:53:43 -0700

    created sql data generation script

--- createMoreData.js
+++ createMoreData.js
@@ -1,12 +1,24 @@
 const fs = require("fs");
 const faker = require("faker");
 const img = require("./server/images.js");
-let dataStream = fs.createWriteStream("data.csv", {
+//homes
+let dataStream = fs.createWriteStream("data2.csv", {
+  encoding: "utf8",
+  flag: "a"
+});
+//listings
+let dataStream2 = fs.createWriteStream("data.csv", {
+  encoding: "utf8",
+  flag: "a"
+});
+//users
+let dataStream3 = fs.createWriteStream("data1.csv", {
   encoding: "utf8",
   flag: "a"
 });
 
-const writeMoreHomes = () => {
+//write main homes
+async function writeMoreHomes() {
   const houseType = [
     "ENTIRE HOUSE",
     "ENTIRE APARTMENT",
@@ -16,15 +28,17 @@ const writeMoreHomes = () => {
   const description = [
     "Cozy house in friendly neiborhood",
     "Spacious apartment",
-    "Sunny, Modern room",
+    "Sunny Modern room",
     "Penthouse Studio",
     "Perfect Weekender"
   ];
   dataStream.write(
-    "id, img, type, address, description, cost, rating, votes\n"
+    "id, img, type, address, description, cost, rating, votes, users\n"
   );
-  for (let i = 1; i <= 1000; i += 1) {
+  for (let i = 1; i <= 10000000; i += 1) {
     let text =
+      i +
+      "," +
       img.getImg() +
       " , " +
       houseType[Math.floor(Math.random() * houseType.length)] +
@@ -37,27 +51,76 @@ const writeMoreHomes = () => {
       "," +
       (Math.random() * (5 - 0) + 0).toFixed(2) +
       "," +
-      faker.random.number({ min: 0, max: 3500 });
-    dataStream.write(text + "\n");
+      faker.random.number({ min: 0, max: 3500 }) +
+      "," +
+      Math.floor(Math.random() * 1e7);
+    let writeHome = dataStream.write(text + "\n");
+    if (!writeHome) {
+      await new Promise(resolve => {
+        dataStream.once("drain", resolve);
+      });
+    }
     if (i % 100000 === 0) {
       console.log(i);
     }
   }
   dataStream.end();
   console.log("done");
-};
+}
 
-// const writeListings = () => {};
+//write listings
+// const writeListings = () => {
+async function writeListings() {
+  dataStream2.write("id, listings\n");
+  for (let i = 1; i < 10000000; i++) {
+    for (let j = 1; j <= 15; j++) {
+      let listing = (i - 1) * 15 + j;
+      let joined = i + " , " + Math.floor(Math.random() * 1e7);
+      let writeList = dataStream2.write(joined + "\n");
+      if (!writeList) {
+        await new Promise(resolve => {
+          dataStream2.once("drain", resolve);
+        });
+      }
+    }
+    if (i % 100000 === 0) {
+      console.log(i);
+    }
+  }
+  dataStream2.end();
+  console.log("done");
+}
+
+async function writeUsers() {
+  dataStream3.write("id, name, email\n");
+  for (let i = 1; i < 10000000; i++) {
+    let users = i + "," + faker.name.findName() + "," + faker.internet.email();
+    let writeUser = dataStream3.write(users + "\n");
+    if (!writeUser) {
+      await new Promise(resolve => {
+        dataStream3.once("drain", resolve);
+      });
+    }
+    if (i % 1000000 === 0) {
+      console.log(i);
+    }
+  }
+  dataStream3.end();
+  console.log("done");
+}
 
 //create homes table
-writeMoreHomes();
+writeMoreHomes(); //complete data in data2.csv
 
 //create listings relationship table
+// writeListings(); // complete data in data.csv
 
-// id, img, type, address, description, cost, rating, votes
-// 1, https://s3-us-west-1.amazonaws.com/homes-pic/12.jpg,ENTIRE HOUSE,Calistafort,Perfect Weekender,5767,4.68,2768
+//create users who own each home
+// writeUsers();
 
 // CREATE TABLE homes (id int NOT NULL,img text,type text,address text,description text,price int, rating numeric(1, 2), votes int)
 
 // id, img, type, address, description, cost, rating, votes
 // 1, https://s3-us-west-1.amazonaws.com/homes-pic/1.jpg , PRIVATE ROOM,Bernierfort,Perfect Weekender,2440,4.55,546
+
+// COPY test(img, type, address, description, price, rating, votes) FROM '/Users/Jimmy/hack-reactor/sdc/premium-more-homes-jl/data.csv' CSV HEADER;
